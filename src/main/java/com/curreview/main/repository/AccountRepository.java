69a3ff29f59fa3a16a8fbbbe8499ef7fb898a70f
h2__gon <h2__gon@naver.com>
2019-05-12 17:35:00 +0900

    project setting

---
+++ src/main/java/com/curreview/main/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.curreview.main.repository;
+
+import com.curreview.main.Model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Long> {
+
+}
